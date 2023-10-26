a1b6452b18aff23a7bf0e8c7e27f2eb02041bcb2
koke2001n <kouken0419komatu@gmail.com>
2023-10-26 16:39:54 +0900

    本当に長かった

--- app/index.py
+++ app/index.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "<h1>Hello, Flask!</h1>"
+  return "<h1>Hello, pipeline!</h1>"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=80, debug=True)
