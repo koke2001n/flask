d4183e36a175c2d457f92f31530ecc937e5a1f7a
koke2001n <kouken0419komatu@gmail.com>
2023-10-27 14:20:44 +0900

    あ

--- app/index.py
+++ app/index.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "<h1>Hello, pipeline!</h1>"
+  return "<h1>Hello, flask_terraform!?</h1>"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=80, debug=True)
